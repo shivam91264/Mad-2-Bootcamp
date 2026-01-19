48a09af68e6fdc165e2f06fbb5fd1a78c6884a11
Shivam Gupta <sg210590@gmail.com>
2026-01-19 17:22:19 +0000

    Session 3 completed

--- backend/applications/api.py
+++ backend/applications/api.py
@@ -1,4 +1,40 @@
-from flask import request
-from flask_restful import Resource
+from flask import request, current_app as app
+from flask_restful import Resource, Api
+from .models import db, Users, Category, Products, Cart, Orders, CategoryRequest
 
 
+class WelcomeAPI(Resource):
+    def get(self):
+        print(request)
+        return {'message': 'Hello, This is GroceryStore!'}, 200
+    
+    def post(self):
+        msg = f'Hello! {request.get_json().get("name")}'
+        return {'message':msg}, 200
+    
+
+
+class LoginAPI(Resource):
+    def post(self):
+        data = request.json
+        user = Users.query.filter_by(email=data.get('email')).first()
+        if user:
+            if user.password == data.get('password'):
+                return {'message': 'User logged in successfully.'}, 200
+            return {'message': 'Incorrect password.'}, 400
+        return {'message': 'User not found.'}, 404
+    
+
+
+class SignupAPI(Resource):
+    def post(self):
+        data = request.json
+        user = Users.query.filter_by(email=data.get('email')).first()
+        if user:
+            return {'message': 'User already exists'}, 400
+        new_user = Users(name=data.get('name'), email=data.get('email'), 
+                        password=data.get('password'), role=data.get('role'))
+        db.session.add(new_user)
+        db.session.commit()
+        return {'message': 'User signup successfully.'}, 201
+        
--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
-from applications.models import db
+from flask_restful import Api
+from applications.models import db, Users
+from applications.api import WelcomeAPI, LoginAPI, SignupAPI
 import os
 
 base_dir = os.path.abspath(os.path.dirname(__file__))
@@ -16,10 +18,27 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.sqlite3"
 
 
 db.init_app(app)
+api = Api(app)
 app.app_context().push()
+
+
+# def add_admin():
+#     admin = Users.query.filter_by(email='admin@gs.com', role = "admin").first()
+#     if not admin:
+#         admin = Users(name="Admin", email='admin@gs.com', password='2030', role = "admin")
+#         db.session.add(admin)
+#         db.session.commit()
+#         return "Admin added"
+
+
+api.add_resource(WelcomeAPI, '/api/welcome')
+api.add_resource(LoginAPI, '/api/login')
+api.add_resource(SignupAPI, '/api/signup')
+
 
 
 if __name__ == '__main__':
     db.create_all()
+    # add_admin()
     app.run(debug=True)
 
